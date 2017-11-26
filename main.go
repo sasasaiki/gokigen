9aa9f4dafa278fc7800e2799f7fa0883a277f064
saiki <otoiawasehakotiradayo@gmail.com>
2017-11-26 19:01:07 +0900

    パッケージ名gokigenに変更
    ご機嫌な感じを出したかったので

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	r := webServer.CreateRoute(webServer.NewProdHandler())
+	r := gokigen.CreateRoute(gokigen.NewProdHandler())
 	//cssやjsを読み込めるようにするHandler
 	r.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("public/"))))
 
--- src/go/handler_func.go
+++ src/go/handler_func.go
@@ -1,4 +1,4 @@
-package webServer
+package gokigen
 
 import (
 	"io"
